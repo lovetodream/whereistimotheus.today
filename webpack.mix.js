50b27b1fd55a11b822677c2263bbca8dd14e849c
Timo <38291523+lovetodream@users.noreply.github.com>
2022-03-01 21:48:22 +0100

    refactor: use livereload instead of browser sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,11 @@
 let mix = require("laravel-mix");
 
-mix.js("Resources/Javascript/app.js", "public/js")
+mix.webpackConfig({
+        plugins: [new LiveReloadPlugin()]
+    })
     .sass("Resources/Styles/app.scss", "public/css")
     .options({
         processCssUrls: false,
         postCss: [require("tailwindcss")]
     })
-    .browserSync({
-        proxy: "http://localhost:8080", 
-        injectChanges: false
-    });
+    .sourceMaps(false);
