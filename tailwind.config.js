5d71f90dbf58cfce3cd486531860beaa5a201821
Timo <38291523+lovetodream@users.noreply.github.com>
2022-03-01 21:51:23 +0100

    feat(style): add firefox variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   mode: 'jit',
   content: ["./Resources/Views/**/*.{leaf,html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant, e, postcss }) {
+      addVariant('firefox', ({ container, separator }) => {
+        const isFirefoxRule = postcss.atRule({
+          name: '-moz-document',
+          params: 'url-prefix()',
+        });
+        isFirefoxRule.append(container.nodes);
+        container.append(isFirefoxRule);
+        isFirefoxRule.walkRules((rule) => {
+          rule.selector = `.${e(
+            `firefox${separator}${rule.selector.slice(1)}`
+          )}`;
+        });
+      });
+    }),
+  ],
 }
