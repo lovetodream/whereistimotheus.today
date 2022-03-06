32651b4d363ef445b28308b9ec9b0dfdcb2f90f3
Timo <38291523+lovetodream@users.noreply.github.com>
2022-03-06 21:41:37 +0100

    style: add eslint

--- Resources/Typescript/app.ts
+++ Resources/Typescript/app.ts
@@ -1,33 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const mapkit: any;
 
 mapkit.init({
-  authorizationCallback: (done: Function) => {
-    fetch("/token")
-      .then(res => res.text())
-      .then(token => done(token))
-      .catch(error => {
-        console.log(error)
-      })
-  }
-})
+    authorizationCallback: (done: (arg0: string) => void) => {
+        fetch("/token")
+            .then(res => res.text())
+            .then(token => done(token))
+            .catch(error => {
+                console.log(error);
+            });
+    }
+});
 
 fetch("/data")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    const center = new mapkit.Coordinate(data.location.latitude, data.location.longitude),
-      span = new mapkit.CoordinateSpan(0.25, 0.25),
-      region = new mapkit.CoordinateRegion(center, span) 
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
 
-    let map = new mapkit.Map("map", {
-      showsCompass: mapkit.FeatureVisibility.Hidden,
-      showsZoomControl: false,
-      showsMapTypeControl: false,
-    })
+        const center = new mapkit.Coordinate(data.location.latitude, data.location.longitude),
+            span = new mapkit.CoordinateSpan(0.25, 0.25),
+            region = new mapkit.CoordinateRegion(center, span),
+            map = new mapkit.Map("map", {
+                showsCompass: mapkit.FeatureVisibility.Hidden,
+                showsZoomControl: false,
+                showsMapTypeControl: false,
+            });
 
-    setTimeout(() => {
-      map.setRegionAnimated(region, true)
-    }, 1000)
+        setTimeout(() => {
+            map.setRegionAnimated(region, true);
+        }, 1000);
 
-  })
-
+    });
+  
