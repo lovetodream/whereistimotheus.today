afbce21ae63a872aa384f7a964ed03bd6f75f4b0
Timo <38291523+lovetodream@users.noreply.github.com>
2022-03-16 19:38:11 +0100

    style: fix linter warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
-const plugin = require('tailwindcss/plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const plugin = require("tailwindcss/plugin");
 
+// eslint-disable-next-line no-undef
 module.exports = {
-  mode: 'jit',
-  content: ["./Resources/Views/**/*.{leaf,html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    plugin(function ({ addVariant, e, postcss }) {
-      addVariant('firefox', ({ container, separator }) => {
-        const isFirefoxRule = postcss.atRule({
-          name: '-moz-document',
-          params: 'url-prefix()',
-        });
-        isFirefoxRule.append(container.nodes);
-        container.append(isFirefoxRule);
-        isFirefoxRule.walkRules((rule) => {
-          rule.selector = `.${e(
-            `firefox${separator}${rule.selector.slice(1)}`
-          )}`;
-        });
-      });
-    }),
-  ],
-}
+    mode: "jit",
+    content: ["./Resources/Views/**/*.{leaf,html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        plugin(function ({ addVariant, e, postcss }) {
+            addVariant("firefox", ({ container, separator }) => {
+                const isFirefoxRule = postcss.atRule({
+                    name: "-moz-document",
+                    params: "url-prefix()",
+                });
+                isFirefoxRule.append(container.nodes);
+                container.append(isFirefoxRule);
+                isFirefoxRule.walkRules((rule) => {
+                    rule.selector = `.${e(
+                        `firefox${separator}${rule.selector.slice(1)}`
+                    )}`;
+                });
+            });
+        }),
+    ],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires, no-undef */
 const mix = require("laravel-mix");
-const LiveReloadPlugin = require('webpack-livereload-plugin');
+const LiveReloadPlugin = require("webpack-livereload-plugin");
 
 mix.webpackConfig({
-        plugins: [new LiveReloadPlugin()]
-    })
+    plugins: [new LiveReloadPlugin()]
+})
     .ts("Resources/Typescript/app.ts", "public/js")
     .sass("Resources/Styles/app.scss", "public/css")
     .options({
